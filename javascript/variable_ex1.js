684bdcd5f609e4518af5187b2d89f01d4ef127e4
mangucletus <66282265+mangucletus@users.noreply.github.com>
2022-11-05 22:09:34 +0000

    javascript variables

---
+++ javascript/variable_ex1.js
@@ -0,0 +1,15 @@
+// swap the values of two variables
+var a = "3";
+var b = "8";
+
+var z = a;
+a = b;
+b = z;
+console.log("a is " + a);
+console.log("b is " + b);
+
+
+// slicing
+var myName = "Cleatus";
+console.log(myName.slice(0, 3));
+console.log(myName.slice(3, 6));
