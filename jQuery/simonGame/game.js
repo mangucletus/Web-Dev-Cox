9e720e804964270704865b5f927236b1fec53ca1
mangucletus <66282265+mangucletus@users.noreply.github.com>
2022-11-13 20:13:03 +0000

    simon game final

--- jQuery/simonGame/game.js
+++ jQuery/simonGame/game.js
@@ -1,11 +1,19 @@
 
-
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 
 var gamePattern = [];
 var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").click(function() {
 
@@ -13,27 +21,59 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
+  animatePress(userChosenColour);
 
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
 
 
-  //1. Inside game.js create a new function called nextSequence()
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 3) + 1; //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
-
-  var randomChosenColour = buttonColours[randomNumber]; // //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
-
-  gamePattern.push(randomChosenColour);   //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
